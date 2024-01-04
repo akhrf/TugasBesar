061af0414fc9223ef19ab53d96129b31afa6e211
akhrf <ahmadkarta9@gmail.com>
2024-01-04 08:58:50 +0700

    .

--- kelompok 1/halaman-login.php
+++ kelompok 1/halaman-login.php
@@ -1,10 +1,1 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
 
-if ($username == 'user' && $password == 'pass') {
-    echo "Login berhasil!";
-} else {
-    echo "Login gagal. Silakan coba lagi.";
-}
-?>
